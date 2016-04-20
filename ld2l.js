7321a7f9aa19496bffa778462a79a22e8a065cbd
Alex Muench <ammuench@gmail.com>
2016-04-20 00:52:15 -0500

    Some basic regex for grabbing game schedules

--- ld2l.js
+++ ld2l.js
@@ -1,40 +1,44 @@
-/*
-	this bot is a ping pong bot, and every time a message
-	beginning with "ping" is sent, it will reply with
-	"pong!".
-*/
+/**
+ * LD2L Bot
+ */
 
-var Discord = require("./node_modules/discord.js");
+//Require needs deps and auth file
+var Discord = require('./node_modules/discord.js');
+var AuthDetails = require('./auth.json');
 
-// Get the email and password
-var AuthDetails = require("./auth.json");
-
+//create bot
 var bot = new Discord.Client();
 
-//when the bot is ready
-bot.on("ready", function () {
-	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+//Bot ready code
+bot.on('ready', function () {
+	console.log('Ready to begin! Serving in ' + bot.channels.length + ' channels');
 });
 
-//when the bot disconnects
-bot.on("disconnected", function () {
-	//alert the console
-	console.log("Disconnected!");
-
-	//exit node.js with an error
+//Bot disconnect code
+bot.on('disconnected', function () {
+	console.log('Disconnected!');
+	//exit node.js
 	process.exit(1);
 });
 
-//when the bot receives a message
-bot.on("message", function (msg) {
-	//if message begins with "ping"
-	if (msg.content.indexOf("ping") === 0) {
-		//send a message to the channel the ping message was sent in.
-		bot.sendMessage(msg.channel, "pong!");
+//Bot logic to run whenever a message is received
+bot.on('message', function (msg) {
 
-		//alert the console
-		console.log("pong-ed " + msg.author.username);
+	console.log('Message from channel : ' + msg.channel.name);
+	if (msg.content.indexOf('!bot schedule') === 0){
+		var rawData = msg.content;
+		var scheduleInfo = {};
+		scheduleInfo.team1 = /(?=Team 1:).+/.exec(rawData)[0].split('Team 1:')[1];
+		scheduleInfo.team2 = /(?=Team 2:).+/.exec(rawData)[0].split('Team 2:')[1];
+		scheduleInfo.time = /(?=Time:).+/.exec(rawData)[0].split('Time:')[1];
+		console.log(scheduleInfo.team1 + " will play " + scheduleInfo.team2 + " on " + scheduleInfo.time);
 	}
+	if (msg.content.indexOf('!bot help') === 0){
+		var helpMsg= "Hi, I'm LD2L Bot!  To schedule a match, please make a post with the following structure: \n!bot schedule\nTeam 1: [Team Name]\nTeam 2: [Team Name]\nTime: [MM/DD/YYYY HH:MM(AM/PM)(TMZ)";
+		bot.sendMessage(msg.channel, helpMsg);
+	}
+
 });
 
+//Login with auth.json
 bot.login(AuthDetails.email, AuthDetails.password);
