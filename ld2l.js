400714c888269e913c172a6a3ff4bb764f607569
Hiemanshu Sharma <hiemanshu@gmail.com>
2016-04-20 19:55:11 +0530

    Clean up code. Setup a command predecessor

--- ld2l.js
+++ ld2l.js
@@ -6,6 +6,8 @@
 var Discord = require('./node_modules/discord.js');
 var AuthDetails = require('./auth.json');
 
+var commandPredecessor = "!";
+
 //create bot
 var bot = new Discord.Client();
 
@@ -25,20 +27,42 @@ bot.on('disconnected', function () {
 bot.on('message', function (msg) {
 
 	console.log('Message from channel : ' + msg.channel.name);
-	if (msg.content.indexOf('!bot schedule') === 0){
-		var rawData = msg.content;
-		var scheduleInfo = {};
-		scheduleInfo.team1 = /(?=Team 1:).+/.exec(rawData)[0].split('Team 1:')[1];
-		scheduleInfo.team2 = /(?=Team 2:).+/.exec(rawData)[0].split('Team 2:')[1];
-		scheduleInfo.time = /(?=Time:).+/.exec(rawData)[0].split('Time:')[1];
-		console.log(scheduleInfo.team1 + " will play " + scheduleInfo.team2 + " on " + scheduleInfo.time);
-	}
-	if (msg.content.indexOf('!bot help') === 0){
-		var helpMsg= "Hi, I'm LD2L Bot!  To schedule a match, please make a post with the following structure: \n!bot schedule\nTeam 1: [Team Name]\nTeam 2: [Team Name]\nTime: [MM/DD/YYYY HH:MM(AM/PM)(TMZ)";
-		bot.sendMessage(msg.channel, helpMsg);
-	}
 
+	message = msg.content;
+	if (message.indexOf(commandPredecessor) === 0) {
+		command = message.split(" ")[0].toLowerCase().substr(1);
+		console.log("\"" + command + "\"")
+		switch (command) {
+			case "schedule":
+				scheduleMatch(message);
+				break;
+			case "help":
+				showHelp(msg.channel);
+				break;
+			default:
+				showInvalidCommand(msg.channel);
+		}
+
+	}
 });
 
 //Login with auth.json
 bot.login(AuthDetails.email, AuthDetails.password);
+
+function scheduleMatch(message) {
+	var scheduleInfo = {};
+	scheduleInfo.team1 = /(?=Team 1:).+/.exec(message)[0].split('Team 1:')[1];
+	scheduleInfo.team2 = /(?=Team 2:).+/.exec(message)[0].split('Team 2:')[1];
+	scheduleInfo.time = /(?=Time:).+/.exec(message)[0].split('Time:')[1];
+	console.log(scheduleInfo.team1 + " will play " + scheduleInfo.team2 + " on " + scheduleInfo.time);
+}
+
+function showInvalidCommand(channel) {
+	var invalidCommandMsg = "That was a invalid command. Try again. Use !help for help."
+	bot.sendMessage(channel, invalidCommandMsg);
+}
+
+function showHelp(channel) {
+	var helpMsg= "Hi, I'm LD2L Bot!  To schedule a match, please make a post with the following structure: \n!schedule\nTeam 1: [Team Name]\nTeam 2: [Team Name]\nTime: [MM/DD/YYYY HH:MM(AM/PM)(TMZ)";
+	bot.sendMessage(channel, helpMsg);
+}
